9e90901ec9625e05363acd1e07c21d46f6ba8741
Anuj Tanwar <91279830+anujtanwar98@users.noreply.github.com>
2024-11-11 13:18:29 -0500

    firebase config template file and luxon

---
+++ auth/template.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "auth-domain",
+  projectId: "project-id",
+  storageBucket: "storage-bucket",
+  messagingSenderId: "messaging-sender-id",
+  appId: "app-id",
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Initialize Auth with AsyncStorage persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+const db = getFirestore(app);
+
+export { auth, db};
